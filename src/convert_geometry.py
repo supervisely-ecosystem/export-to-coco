522a7fe2ffd009af89fc52cbfe8693e1e25453f7
GoldenAnpu <seryios.sych@gmail.com>
2023-07-31 19:22:40 +0200

    add rectangle mark; add NotImplementedError handler for unsupported geometries;

--- src/convert_geometry.py
+++ src/convert_geometry.py
@@ -1,5 +1,8 @@
 import supervisely as sly
 from supervisely.annotation.json_geometries_map import GET_GEOMETRY_FROM_STR
+from supervisely.geometry import rectangle
+from globals import rectangle_mark
+from supervisely.sly_logger import logger
 
 
 def prepare_meta(meta):
@@ -15,10 +18,22 @@ def prepare_meta(meta):
 def convert_annotation(ann: sly.Annotation, dst_meta):
     new_labels = []
     for lbl in ann.labels:
-        new_cls = dst_meta.obj_classes.get(lbl.obj_class.name)
-        if lbl.obj_class.geometry_type == new_cls.geometry_type:
-            new_labels.append(lbl)
-        else:
-            converted_labels = lbl.convert(new_cls)
-            new_labels.extend(converted_labels)
+        try:
+            new_cls = dst_meta.obj_classes.get(lbl.obj_class.name)
+            if lbl.obj_class.geometry_type == new_cls.geometry_type:
+                new_labels.append(lbl)
+            else:
+                converted_label = lbl.convert(new_cls)
+                if lbl.obj_class.geometry_type == rectangle.Rectangle:
+                    new_descr = converted_label[0].description + " " + rectangle_mark
+                    new_label = converted_label[0].clone(description=new_descr)
+                    converted_label.pop()
+                    converted_label.append(new_label)
+                new_labels.extend(converted_label)
+        except NotImplementedError as e:
+            logger.critical(
+                f"Unsupported conversion of annotation '{lbl.obj_class.geometry_type.name()}' type to '{new_cls.geometry_type.name()}'. Skipping annotation with [ID: {lbl.to_json()['id']}]",
+                exc_info=False,
+            )
+            continue
     return ann.clone(labels=new_labels)
